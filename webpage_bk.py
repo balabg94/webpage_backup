e648dd1c8b876d2a68049b8379134778388c19a8
Balagovind <bala@hamon.in>
2019-11-15 16:06:44 +0530

    Removed hardcoded path to the recovery file
    The path to the recovery file was hard coded
    Removed that using a python module named fnmatch
    Now the file is read dynamically everytime the script is run.

--- webpage_bk.py
+++ webpage_bk.py
@@ -2,16 +2,25 @@
 
 from datetime import datetime
 import argparse
+import fnmatch
 import json
 import lz4.block
 import os
 import sys
 
-os.environ['opentabs'] = '/home/bg/.mozilla/firefox/2dmmjsje.default-1569575256935/sessionstore-backups/recovery.jsonlz4'
-firefox_recovery_file= open(os.environ["opentabs"], "rb")
+home = os.path.expanduser("~")
+firefox_path = os.path.join(home,".mozilla/firefox")
+
+def recovery_path(firefox_path):
+    for file in os.listdir(firefox_path):
+        if fnmatch.fnmatch(file, '*.default*'):
+            new_path = os.path.join(firefox_path, file, 'sessionstore-backups/recovery.jsonlz4')
+            break
+    return new_path
+
 now_obj= datetime.now()
 now= now_obj.strftime("%b %d %Y %k:%M:%S")
-
+    
 def backup_urls():
     magic = firefox_recovery_file.read(8)
     jdata = json.loads(lz4.block.decompress(firefox_recovery_file.read()).decode("utf-8"))
@@ -20,23 +29,22 @@ def backup_urls():
         for tab in win.get("tabs"):
             i = int(tab.get("index")) - 1
             urls.append(tab.get("entries")[i].get("url"))
-    url_new = write_file(now, urls)
+    url_new = write_file(urls)
     return 'Done. Recorded {} urls'.format(url_new)
 
-def write_file(now, urls):
+def write_file(urls):
     backup= open('webpage_bk.bk', 'a+')
     backup.seek(0)
     content= backup.read()
     #backup.write('[{}]\n'.format(now))
     for i in urls:
-#        import pdb;pdb.set_trace()
         if i not in content:
             backup.write(i+'\n')
         else:
             urls.remove(i)
             pass
-    print(urls)
     return len(urls)
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Application for backing up and reopening weburls')
     parser.add_argument('-n', '--new', default= None, action= 'store_true',  help= 'Backup of current open tabs')
@@ -49,6 +57,9 @@ def parse_args():
     return args
 
 def main():
+    global firefox_recovery_file
+    os.environ['opentabs'] = recovery_path(firefox_path)
+    firefox_recovery_file= open(os.environ["opentabs"], "rb")
     args= parse_args()
     if args.new:
         backup = backup_urls()
